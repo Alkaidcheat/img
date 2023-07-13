bdbf0303148bb84c2430aacdd946e4a4834afcd5
Alkaidcheat <97583063+Alkaidcheat@users.noreply.github.com>
2023-07-14 01:36:15 +0800

    Update api.php

--- api.php
+++ api.php
@@ -4,7 +4,7 @@ $seed = time();
 //获取随机数
 $num = rand(1,45);
 //拼接图片地址
-$picpath = "https://cdn.jsdelivr.net/gh/Alkaidcheat/img/blob/main/".$num.".jpg";
+$picpath = "https://cdn.jsdelivr.net/gh/Alkaidcheat/img/".$num.".jpg";
 //重定位到图片
 die(header("Location: $picpath"));
 ?>
